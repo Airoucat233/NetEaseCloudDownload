19c44672944a820137dd8815ef7205d1a464c3a3
Airoucat <529401274@qq.com>
2021-11-13 23:26:39 +0800

    增加说明

--- GetMusicByListM3U8.py
+++ GetMusicByListM3U8.py
@@ -1,6 +1,9 @@
+"""
+根据一个路径列表(回车符分开),将该列表记载的路径的文件复制到目标目录
+"""
 import os
 import shutil
-f=open(r"F:\零度梦幻\Carplay\车载 1.txt",encoding='utf-8')
+f=open(r"F:\XXXX\XXXX\1.txt",encoding='utf-8')
 s=f.read()
 f.close()
 play_list=s.split('\n')
@@ -8,7 +11,7 @@ print(play_list)
 num=0
 for m in play_list:
     if os.path.exists(m):
-        dst_file=m.replace('./Mp3、','./CarPlayList')
+        dst_file=m.replace('./Mp3','./CarPlayList')
         dst_path=os.path.dirname(dst_file)
         if not os.path.exists(dst_path):
             os.makedirs(dst_path)
@@ -16,5 +19,3 @@ for m in play_list:
         num+=1
         print(m," 成功复制至-> ",dst_path)
 print(f"成功复制 {num} 个文件")
-# os.makedirs('./CarPlayList/参考.py')
-# shutil.copy('./参考.py','./CarPlayList/参考.py')
